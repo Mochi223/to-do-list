2da1199c9ff78c027ed251a7786deb8e06e64820
moses perez <mosesperez556@gmail.com>
2023-05-11 22:49:32 -0500

    new stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import Form from './form';
+import Todo from './Todo';
 
 function App() {
+  const [todos, setTodos] = useState(
+    !localStorage.getItem("todos-list") ?
+      []
+    :
+      JSON.parse(localStorage.getItem("todos-list"))
+  )
+
+  useEffect(() => {
+    localStorage.setItem("todos-list", JSON.stringify(todos))
+  }, [todos])
+
+  function addTodo(newItem){
+    setTodos([...todos, newItem])
+  }
+
+  function markComplete(index){
+    todos[index].isDone = !todos[index].isDone;
+    setTodos([...todos])
+  }
+
+  function deleteOne(index){
+    todos.splice(index, 1);
+    setTodos([...todos])
+  }
+
+  function clearCompleted(){
+    setTodos(todos.filter((todo) => todo.isDone === false));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To do or not to do?</h1>
+      <h2>Make a list of stuff you gotta do today ⬇️ </h2>
+      {/* <br /> */}
+      <h1>🧐👇</h1>
+      <br />
+      <div>
+        {todos.map((todo, index) => {
+          return <Todo key={index} todo={todo} index={index} markComplete={markComplete} deleteOne={deleteOne} />
+        })}
+      </div>
+      <br />
+      <Form addTodo={addTodo} clearCompleted={clearCompleted} />
     </div>
   );
 }
---
+++ src/Todo.js
@@ -0,0 +1,44 @@
+import { Container, IconButton, Typography } from "@mui/material";
+import { Delete, Done, Cancel } from "@mui/icons-material";
+
+function Todo({todo, markComplete, index, deleteOne}) {
+
+    const containerStyle = {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        border: "1px solid white",
+        borderRadius: "5px",
+        height: "100%",
+      }
+
+    return (  
+        <Container style={containerStyle}>
+
+            <IconButton onClick={() => markComplete(index)} style={{ color: "#50fa7b" }}>
+                {todo.isDone ? <Cancel></Cancel> : <Done></Done>}
+            </IconButton>
+
+            <Typography
+                variant="h5"
+                style={
+                    todo.isDone ?
+                    {
+                        textDecoration: "line-through",
+                        color: "gray"
+                    }
+                    :
+                    {}
+                }
+            >
+                {todo.content}
+            </Typography>
+
+            <IconButton style={{ color: "#ff5555" }} onClick={() => deleteOne(index)}>
+                <Delete></Delete>
+            </IconButton>
+        </Container>
+    );
+}
+
+export default Todo;
